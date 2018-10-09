b2096f41b464cb04c1cd86c425be8169a5f3c74d
bananu7 <bananu7@o2.pl>
2018-10-09 11:11:40 +0200

    Renamed test to examples and added random_pixels

---
+++ examples/random_pixels.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <array>
+#include <cstdint>
+
+#include "OpenGLWindow.hpp"
+#include <GL/GL.h>
+
+int main() {
+    try {
+        oglw::Window win("Window", 800, 600);
+
+        win.keydownCallback = [&win](oglw::KeyInfo const& k) { printf("lol"); };
+
+        const std::array<uint8_t, 3> color{ 255, 0, 0 };
+        std::vector<std::pair<int, int>> pixels; 
+
+        for (int i = 0; i < 500; ++i) {
+            const int x = rand() / (double)RAND_MAX * win.getSizeX();
+            const int y = rand() / (double)RAND_MAX * win.getSizeY();
+            pixels.push_back({ x, y });
+        }
+
+        glDisable(GL_DEPTH_TEST);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        glOrtho(0, 800, 600, 0, -1, 1);
+
+        win.displayFunc = [&]() {
+            for (auto const& pixel : pixels) {
+                glRasterPos2i(pixel.first, pixel.second);
+                glDrawPixels(1, 1, GL_RGB, GL_UNSIGNED_BYTE, color.data());
+            }
+        };
+
+        while (win.display(), win.process());
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what();
+    }
+}
